aab5b676ff45f9819de95432d88195e3f2845849
ProphetLamb <rene.carannante@outlook.de>
2020-05-25 00:42:16 +0200

    Open file by 'Uri' via 'AbolutePath' property

--- CrossoutLogViewer.Common/ExplorerOpenFile.cs
+++ CrossoutLogViewer.Common/ExplorerOpenFile.cs
@@ -8,13 +8,26 @@ namespace CrossoutLogView.Common
 {
     public static class ExplorerOpenFile
     {
+        public static bool OpenFile(Uri fileUri)
+        {
+            return OpenFile(fileUri.AbsolutePath);
+        }
+        
         public static bool OpenFile(string filePath)
         {
             if (!File.Exists(filePath)) return false;
-            using var opener = new Process();
-            opener.StartInfo.FileName = "explorer";
-            opener.StartInfo.Arguments = "\"" + Path.GetFullPath(filePath) + "\"";
-            opener.Start();
+            try
+            {
+                using var opener = new Process();
+                opener.StartInfo.FileName = "explorer";
+                opener.StartInfo.Arguments = "\"" + Path.GetFullPath(filePath) + "\"";
+                opener.Start();
+            }
+            catch (Exception ex)
+            {
+                Logging.WriteLine(ex);
+                return false;
+            }
             return true;
         }
     }
