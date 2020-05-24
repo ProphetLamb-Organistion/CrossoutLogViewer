c30ad0734ec29bfcadc8f24346f7a3f3861008ce
ProphetLamb <rene.carannante@outlook.de>
2020-05-25 01:08:03 +0200

    New class 'WindowViewModel', new event 'ColorWindowTitlebar' - WindowViewModel:  base class for ViewModels for Windows; natively implements the 'ColorWindowTitlebar' property shared by all 'Window's . - ColorWindowTitlebarEvent: Event used to comunicate a change to the above mentioned property.

---
+++ CrossoutLogViewer.GUI/Core/WindowViewModel.cs
@@ -0,0 +1,39 @@
+﻿using CrossoutLogView.Database.Data;
+using CrossoutLogView.GUI.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrossoutLogView.GUI.Core
+{
+    public class WindowViewModel : ViewModelBase
+    {
+        private bool _colorWindowTitlebar = Settings.Current.ColorWindowTitlebar;
+
+        public static ColorWindowTitlebarEventHandler ColorWindowTitlebarChanged;
+
+        public WindowViewModel()
+        {
+            ColorWindowTitlebarChanged += OnColorWindowTitlebarChanged;
+        }
+
+        private void OnColorWindowTitlebarChanged(object sender, ColorWindowTitlebarEventArgs e)
+        {
+            _colorWindowTitlebar = e.NewValue;
+        }
+
+        public bool ColorWindowTitlebar
+        {
+            get => _colorWindowTitlebar;
+            set
+            {
+                var oldValue = _colorWindowTitlebar;
+                Set(ref _colorWindowTitlebar, value);
+                Settings.Current.ColorWindowTitlebar = value;
+                ColorWindowTitlebarChanged?.Invoke(this, new ColorWindowTitlebarEventArgs(oldValue, value));
+            }
+        }
+
+        public override void UpdateCollections() { }
+    }
+}
