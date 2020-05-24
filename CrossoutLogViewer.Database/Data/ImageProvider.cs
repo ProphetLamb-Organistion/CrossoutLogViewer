3836351cd33c85542a6794956383690e9407f061
ProphetLamb <rene.carannante@outlook.de>
2020-05-25 00:52:27 +0200

    Add class 'ImageProvider' - GetMapImageUri: Returns a Uri pointing to a map image as specified by the 'mapName' parameter.

---
+++ CrossoutLogViewer.Database/Data/ImageProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrossoutLogView.Database.Data
+{
+    public static class ImageProvider
+    {
+        public static Uri GetMapImageUri(string mapName)
+        {
+            return new Uri(Environment.CurrentDirectory + "//images//" + mapName.Trim() + ".jpg", UriKind.Absolute);
+        }
+    }
+}
