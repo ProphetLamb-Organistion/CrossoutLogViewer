8e16765ef0f6146ce40c08feae59a1777acf7c9c
ProphetLamb <rene.carannante@outlook.de>
2020-06-09 21:24:53 +0200

    Add DateTimeExtention.cs - Extention function StartOfWeek - Extention function StartOfMonth - Extention function EndOfMonth

---
+++ CrossoutLogViewer.Common/DateTimeExtention.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrossoutLogView.Common
+{
+    public static class DateTimeExtention
+    {
+        public static DateTime StartOfWeek(this DateTime dt, DayOfWeek startOfWeek = DayOfWeek.Monday)
+        {
+            int diff = (7 + (dt.DayOfWeek - startOfWeek)) % 7;
+            return dt.AddDays(-1 * diff).Date;
+        }
+
+        public static DateTime StartOfMonth(this DateTime dt)
+        {
+            return new DateTime(dt.Year, dt.Month, 1);
+        }
+
+        public static DateTime EndOfMonth(this DateTime dt)
+        {
+            return dt.StartOfMonth().AddDays(DateTime.DaysInMonth(dt.Year, dt.Month)).AddSeconds(-1);
+        }
+    }
+}
