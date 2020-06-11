7f3e9cac27cbb4b6216c9f9de46f05d2806cece9
ProphetLamb <rene.carannante@outlook.de>
2020-06-11 16:35:38 +0200

    ViewModel for UserListControl

---
+++ CrossoutLogViewer.GUI/Models/UsersListControlModel.cs
@@ -0,0 +1,15 @@
+﻿using CrossoutLogView.GUI.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrossoutLogView.GUI.Models
+{
+    public class UsersListControlModel : ViewModelBase
+    {
+        private string _userName;
+        public string FilterUserName { get => _userName; set => Set(ref _userName, value?.TrimStart()); }
+
+        public override void UpdateCollections() => throw new InvalidOperationException();
+    }
+}
