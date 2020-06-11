1841736c16d933db1e7074be087aa2c0a7e3b607
ProphetLamb <rene.carannante@outlook.de>
2020-06-11 16:23:47 +0200

    Add and Remove extentions for ConcurrentDictonary

---
+++ CrossoutLogViewer.Common/ConcurrentDictionaryExtention.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrossoutLogView.Common
+{
+    public static class ConcurrentDictionaryExtention
+    {
+        public static void Add<TKey, TValue>(this ConcurrentDictionary<TKey, TValue> dictionary, TKey key, TValue value)
+        {
+            dictionary.AddOrUpdate(key, value, (k, oldValue) => value);
+        }
+
+        public static bool Remove<TKey, TValue>(this ConcurrentDictionary<TKey,TValue> dictionary, TKey key)
+        {
+            return dictionary.TryRemove(key, out _);
+        }
+    }
+}
