4a631fb629393f350c4828cdf58706fd866c444d
ProphetLamb <rene.carannante@outlook.de>
2020-06-11 16:37:29 +0200

    Miscellaneous

--- CrossoutLogViewer.Test/Program.cs
+++ CrossoutLogViewer.Test/Program.cs
@@ -4,9 +4,11 @@ using CrossoutLogView.Database.Collection;
 using CrossoutLogView.Database.Connection;
 using CrossoutLogView.Database.Data;
 using CrossoutLogView.GUI.Core;
+using CrossoutLogView.GUI.Models;
 using CrossoutLogView.Log;
 
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 
@@ -16,23 +18,6 @@ namespace CrossoutLogView.Test
     {
         public static void Main()
         {
-            var col = new ObservableCollection<int>() 
-            {
-                1,
-                5,
-                6,
-                3,
-                0,
-                2,
-                4
-            };
-            col.Sort((x,y) => y.CompareTo(x));
-            Console.WriteLine("Sort");
-            foreach (var v in col) Console.WriteLine(v);
-            Console.WriteLine("Find");
-            Console.WriteLine(col.Find(x => x == 4));
-            Console.WriteLine("FindIndex");
-            Console.WriteLine(col.FindIndex(x => x == 4));
             Console.ReadLine();
         }
     }
