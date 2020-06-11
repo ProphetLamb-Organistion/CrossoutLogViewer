30bfdccaebff5345deafad9fa46b07e2c9019db6
ProphetLamb <rene.carannante@outlook.de>
2020-06-11 16:31:10 +0200

    Implement a two content control redirecting scoll events

---
+++ CrossoutLogViewer.GUI/Controls/ScrollableHeaderedControl.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CrossoutLogView.GUI.Controls
+{
+    [DefaultProperty(nameof(Content))]
+    /// <summary>
+    /// Interaction logic for ScrollableHeaderControl.xaml
+    /// </summary>
+    public partial class ScrollableHeaderedControl : UserControl
+    {
+        public ScrollableHeaderedControl()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Gets or sets the header used to generate the content of the <see cref="ScrollableHeaderedControl"/>.
+        /// </summary>
+        public object HeaderContent { get => GetValue(HeaderContentProperty) as object; set => SetValue(HeaderContentProperty, value); }
+        public static readonly DependencyProperty HeaderContentProperty = DependencyProperty.Register(nameof(HeaderContent), typeof(object), typeof(ScrollableHeaderedControl));
+
+        /// <summary>
+        /// Gets or sets the content used to generate the content of the <see cref="ScrollableHeaderedControl"/>.
+        /// </summary>
+        public new object Content { get => GetValue(ContentProperty) as object; set => SetValue(ContentProperty, value); }
+        public static new readonly DependencyProperty ContentProperty = DependencyProperty.Register(nameof(Content), typeof(object), typeof(ScrollableHeaderedControl));
+
+        private void ContentPresenter_Content_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            var args = new MouseWheelEventArgs(e.MouseDevice, e.Timestamp, e.Delta);
+            args.RoutedEvent = ScrollViewer.MouseWheelEvent;
+            ScrollViewerMain.RaiseEvent(args);
+        }
+    }
+}
