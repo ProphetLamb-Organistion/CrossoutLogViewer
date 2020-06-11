3cbcf67a7f35a4696790f3d9825c664aa79ebbc0
ProphetLamb <rene.carannante@outlook.de>
2020-06-11 16:34:30 +0200

    Enum defining what dimesions the Chart displays

---
+++ CrossoutLogViewer.GUI/Core/Dimensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CrossoutLogView.GUI.Models
+{
+    [Flags]
+    public enum Dimensions
+    {
+        Score =                 1 << 0,
+        Kills =                 1 << 1,
+        Assists =               1 << 2,
+        Deaths =                1 << 3,
+        ArmorDamageDealt =      1 << 4,
+        CriticalDamageDealt =   1 << 5,
+        ArmorDamageTaken =      1 << 6,
+        CriticalDamageTaken =   1 << 7,
+        TotalDamageDealt =      1 << 8,
+        TotalDamageTaken =      1 << 9
+    }
+}
