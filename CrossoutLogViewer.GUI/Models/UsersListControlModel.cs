6ee9105b494b33e7b034c0a8c565748e444b5742
ProphetLamb <rene.carannante@outlook.de>
2020-06-15 23:12:20 +0200

    Multi user name filter

--- CrossoutLogViewer.GUI/Models/UsersListControlModel.cs
+++ CrossoutLogViewer.GUI/Models/UsersListControlModel.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace CrossoutLogView.GUI.Models
@@ -9,6 +10,21 @@ namespace CrossoutLogView.GUI.Models
     public class UsersListControlModel : ViewModelBase
     {
         private string _userName;
-        public string FilterUserName { get => _userName; set => Set(ref _userName, value?.TrimStart()); }
+        private string[] _filtersUserName;
+
+        public string FilterUserName { 
+            get => _userName; 
+            set
+            {
+                var val = value?.TrimStart();
+                if (String.IsNullOrWhiteSpace(val))
+                    FiltersUserName = Array.Empty<string>();
+                else
+                    FiltersUserName = val.Split('|').Where(x => !String.IsNullOrWhiteSpace(x)).Select(x => x.Trim()).ToArray();
+                Set(ref _userName, val);
+            }
+        }
+
+        public string[] FiltersUserName { get => _filtersUserName; set => Set(ref _filtersUserName, value); }
     }
 }
