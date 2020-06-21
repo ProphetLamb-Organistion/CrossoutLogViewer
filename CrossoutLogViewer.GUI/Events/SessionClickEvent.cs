00ff76c117d38f9b4664d86c8d878bc4b46dc602
ProphetLamb <rene.carannante@outlook.de>
2020-06-21 15:54:07 +0200

    Event for click on a session button

---
+++ CrossoutLogViewer.GUI/Events/SessionClickEvent.cs
@@ -0,0 +1,23 @@
+﻿using CrossoutLogView.GUI.Core;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace CrossoutLogView.GUI.Events
+{
+    public delegate void SessionClickEventHandler(object sender, SessionClickEventArgs e);
+
+    public class SessionClickEventArgs : RoutedEventArgs
+    {
+        public SessionClickEventArgs(SessionTimes session, DateTime day) : base()
+        {
+            Session = session;
+            Day = day;
+        }
+
+        public SessionTimes Session { get; set; }
+        public DateTime Day { get; set; }
+    }
+}
