8aafbc23777e0e976b328991a10cff6642ede739
ProphetLamb <rene.carannante@outlook.de>
2020-06-21 15:53:11 +0200

    SessionTime enum for each of the tree ClanWars sessions. - Utility to convert a DateTime to a SessionTime

---
+++ CrossoutLogViewer.GUI/Core/SessionTimes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrossoutLogView.GUI.Core
+{
+    [Flags]
+    public enum SessionTimes
+    {
+        None =      0,
+        Noon =      1 << 0,
+        Afternoon = 1 << 1,
+        Night =     1 << 2
+    }
+
+    public static class SessionTimesHelper
+    {
+        public static SessionTimes GetSessionTimes(this DateTime date) => date.DayOfWeek switch
+        {
+            DayOfWeek.Monday => SessionTimes.Noon,
+            DayOfWeek.Tuesday => SessionTimes.Afternoon,
+            DayOfWeek.Wednesday => SessionTimes.Night,
+            DayOfWeek.Thursday => SessionTimes.Afternoon,
+            DayOfWeek.Friday => SessionTimes.Night,
+            DayOfWeek.Saturday => SessionTimes.Noon | SessionTimes.Afternoon,
+            DayOfWeek.Sunday => SessionTimes.Night | SessionTimes.Noon,
+            _ => SessionTimes.None                    
+        };
+    }
+}
