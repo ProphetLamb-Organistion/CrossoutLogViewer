0ba5bd854a4a77369b820dca6d6034af961c7c02
ProphetLamb <rene.carannante@outlook.de>
2020-06-21 15:53:48 +0200

    Event for changed DateTimes.

---
+++ CrossoutLogViewer.GUI/Events/DateChangedEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrossoutLogView.GUI.Events
+{
+    public delegate void DateChangedEventHandler(object sender, DateChangedEventArgs e);
+
+    public class DateChangedEventArgs : EventArgs
+    {
+        public DateChangedEventArgs(DateTime? oldValue, DateTime? newValue)
+        {
+            OldValue = oldValue;
+            NewValue = newValue;
+        }
+
+        public DateTime? OldValue { get; }
+        public DateTime? NewValue { get; }
+    }
+}
