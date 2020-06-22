3a13690021d52080813a510c4e735bca7947fdcd
ProphetLamb <rene.carannante@outlook.de>
2020-06-22 22:30:50 +0200

    Interface indicating that the implementing class can be merged with another instance.

---
+++ CrossoutLogViewer.Common/IMergable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrossoutLogView.Common
+{
+    public interface IMergable<T>
+    {
+        public T Merge(T other);
+    }
+}
